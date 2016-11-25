bb9594a66d415432b86d2fe45010f1c95678e124
张志豪 <izhangzhihao@hotmail.com>
2016-11-25 17:04:40 +0800

    FIX : template error

--- src/test/java/com/github/izhangzhihao/OfficeProducer/DocxProducerTest.java
+++ src/test/java/com/github/izhangzhihao/OfficeProducer/DocxProducerTest.java
@@ -15,10 +15,10 @@ import static com.github.izhangzhihao.OfficeProducer.DocxProducer.CreateEncryptD
 public class DocxProducerTest {
     @Test
     public void CreateEncryptDocxFromTemplateTest() throws Exception {
-        String templatePath = "/Template/2.docx";
+        String templatePath = "/Template/10.docx";
         HashMap<String, String> parameters = new HashMap<>();
-        parameters.put("colour", "green");
-        parameters.put("icecream", "chocolate");
+        parameters.put("colour", "绿色");
+        parameters.put("icecream", "巧克力");
         HashMap<String, String> imageParameters = new HashMap<>();
         String prefix = "D:/头像/";
         imageParameters.put("bookmark", prefix + "/33.png");
